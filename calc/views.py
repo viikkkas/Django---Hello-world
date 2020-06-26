0ff5f4b77bbf89c55d8eb5b582691ce67041712c
viikkkas <61241966+viikkkas@users.noreply.github.com>
2020-06-26 19:17:53 +0530

    Update views.py

--- calc/views.py
+++ calc/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'batman'})
-
-def add(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
-    res = val1+val2
-    return render(request, 'result.html', {'result':res})
